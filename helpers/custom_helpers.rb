eb6ade19e3f43c045446e9c4172dcb5594a0f26d
Claudio Vallejo <hola@claudiovallejo.mx>
2016-11-23 22:11:04 -0600

    Created a helpers function for a pretty url function.

---
+++ helpers/custom_helpers.rb
@@ -0,0 +1,6 @@
+module CustomHelpers
+  # Pretty URL: Transforms 'José Martí' -> 'jose-marti'
+  def pretty_url string
+    url = I18n.transliterate(string).downcase.strip.gsub(' ', '-')
+  end
+end
