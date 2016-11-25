2d58d5b13214ebe5ca25ebe50a7f5779d4e872a5
Claudio Vallejo <hola@claudiovallejo.mx>
2016-11-24 18:11:08 -0600

    Added comments and created dynamic margin-top script.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,11 +1,27 @@
+/*
+
+  Fade-in Elements on Load
+
+*/
+//
 const loadElements = document.getElementsByClassName('is-hidden');
+//
 window.addEventListener('load', function(){
   for (var i = 0; i < loadElements.length; i++) {
     load(loadElements[i]);
   }
 });
-
+//
 function load(element) {
   element.classList.toggle('is-hidden');
   element.classList.toggle('is-visible');
 }
+/*
+
+  Dynamic Margin-top for Content
+
+*/
+//
+const navHeight = document.getElementsByTagName('nav')[0].offsetHeight;
+const $main = document.getElementsByTagName('main')[0];
+$main.setAttribute('style', 'margin-top: ' + navHeight + 'px;');
