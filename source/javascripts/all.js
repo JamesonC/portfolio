c636c203fbdd3b11d5a7b8c26610053c24becdf7
Claudio Vallejo <hola@claudiovallejo.mx>
2016-12-19 12:08:28 -0600

    Re-structured navigation menu, styled case study source-links.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,6 @@
 /*
 
-  Fade-in Elements on Load
+  Document Load-in Animation
 
 */
 //
@@ -13,7 +13,7 @@ window.addEventListener('load', function(){
 });
 /*
 
-  Fade-in Elements on Load
+  Card Link Interactions
 
 */
 const links = document.getElementsByClassName('js-link');
@@ -23,7 +23,7 @@ for (var l = 0; l < links.length; l++) {
   links[l].addEventListener('mouseenter', function(){
     var $title = this.getElementsByClassName('js-title')[0];
     var $subtitle = this.getElementsByClassName('js-title')[1];
-    move(this);
+    translateY(this);
     shadow(this);
     show($title);
     show($subtitle);
@@ -32,19 +32,54 @@ for (var l = 0; l < links.length; l++) {
   links[l].addEventListener('mouseleave', function(){
     var $title = this.getElementsByClassName('js-title')[0];
     var $subtitle = this.getElementsByClassName('js-title')[1];
-    move(this);
+    translateY(this);
     shadow(this);
     show($title);
     show($subtitle);
   });
 }
+/*
+
+  Case Study Source Links
+
+*/
+const sources = document.getElementsByClassName('js-source');
+//
+for (var s = 0; s < sources.length; s++) {
+  //
+  sources[s].addEventListener('mouseenter', function(){
+    translateY(this);
+    shadow(this);
+  });
+  //
+  sources[s].addEventListener('mouseleave', function(){
+    translateY(this);
+    shadow(this);
+  });
+}
+/*
+
+  Navigation Arrow Interaction
+
+*/
+const nav = document.getElementsByClassName('js-nav')[0];
+//
+nav.addEventListener('mouseenter', function(){
+  var $arrow = this.getElementsByClassName('js-arrow')[0];
+  translateX($arrow);
+});
+//
+nav.addEventListener('mouseleave', function(){
+  var $arrow = this.getElementsByClassName('js-arrow')[0];
+  translateX($arrow);
+});
 //
 function show($element) {
   $element.classList.toggle('opacity-0');
   $element.classList.toggle('opacity-1');
 }
 //
-function move($element) {
+function translateY($element) {
   $element.classList.toggle('translateY-0');
   $element.classList.toggle('translateY--0_5');
 }
@@ -52,4 +87,9 @@ function move($element) {
 function shadow($element) {
   $element.classList.toggle('shadow-0');
   $element.classList.toggle('shadow-1');
+}
+//
+function translateX($element) {
+  $element.classList.toggle('translateX-0');
+  $element.classList.toggle('translateX--0_5');
 }
