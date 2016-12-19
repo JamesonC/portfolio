4877769b0a5e04d55a386153de36d6d9ba864779
Claudio Vallejo <hola@claudiovallejo.mx>
2016-12-18 22:03:16 -0600

    Started working on index card interactions.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -16,3 +16,31 @@ function load(element) {
   element.classList.toggle('is-hidden');
   element.classList.toggle('is-visible');
 }
+/*
+
+  Fade-in Elements on Load
+
+*/
+const links = document.getElementsByClassName('js-link');
+//
+for (var l = 0; l < links.length; l++) {
+  //
+  links[l].addEventListener('mouseenter', function(){
+    var $title = this.getElementsByClassName('js-title')[0];
+    var $subtitle = this.getElementsByClassName('js-title')[1];
+    set($title);
+    set($subtitle);
+  });
+  //
+  links[l].addEventListener('mouseleave', function(){
+    var $title = this.getElementsByClassName('js-title')[0];
+    var $subtitle = this.getElementsByClassName('js-title')[1];
+    set($title);
+    set($subtitle);
+  });
+}
+//
+function set($title) {
+  $title.classList.toggle('is-hidden');
+  $title.classList.toggle('is-visible');
+}
