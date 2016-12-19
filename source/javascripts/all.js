b3b46aaae6e6d880741c9921dffea8c90d0986ea
Claudio Vallejo <hola@claudiovallejo.mx>
2016-12-19 10:58:08 -0600

    Re-styled interactive cards, re-structured animation classes.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,18 +4,13 @@
 
 */
 //
-const loadElements = document.getElementsByClassName('is-hidden');
+const loadElements = document.getElementsByClassName('js-load');
 //
 window.addEventListener('load', function(){
   for (var i = 0; i < loadElements.length; i++) {
-    load(loadElements[i]);
+    show(loadElements[i]);
   }
 });
-//
-function load(element) {
-  element.classList.toggle('is-hidden');
-  element.classList.toggle('is-visible');
-}
 /*
 
   Fade-in Elements on Load
@@ -28,19 +23,33 @@ for (var l = 0; l < links.length; l++) {
   links[l].addEventListener('mouseenter', function(){
     var $title = this.getElementsByClassName('js-title')[0];
     var $subtitle = this.getElementsByClassName('js-title')[1];
-    set($title);
-    set($subtitle);
+    move(this);
+    shadow(this);
+    show($title);
+    show($subtitle);
   });
   //
   links[l].addEventListener('mouseleave', function(){
     var $title = this.getElementsByClassName('js-title')[0];
     var $subtitle = this.getElementsByClassName('js-title')[1];
-    set($title);
-    set($subtitle);
+    move(this);
+    shadow(this);
+    show($title);
+    show($subtitle);
   });
 }
 //
-function set($title) {
-  $title.classList.toggle('is-hidden');
-  $title.classList.toggle('is-visible');
+function show($element) {
+  $element.classList.toggle('opacity-0');
+  $element.classList.toggle('opacity-1');
+}
+//
+function move($element) {
+  $element.classList.toggle('translateY-0');
+  $element.classList.toggle('translateY--0_5');
+}
+//
+function shadow($element) {
+  $element.classList.toggle('shadow-0');
+  $element.classList.toggle('shadow-1');
 }
