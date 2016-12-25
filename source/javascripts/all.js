0364e0c2ccd4b246570513b2a46e9a7d44b399b3
Claudio <hola@claudiovallejo.mx>
2016-12-25 12:11:18 -0600

    Restructured scripts.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,94 +4,73 @@
 
 */
 //
-const loadElements = document.getElementsByClassName('js-load');
+const $load = document.getElementsByClassName('js-load');
 //
 window.addEventListener('load', function(){
-  for (var i = 0; i < loadElements.length; i++) {
-    show(loadElements[i]);
+  if ($load) {
+    for (var i = 0; i < $load.length; i++) {
+      toggle($load[i], 'opacity-0', 'opacity-1');
+    }
   }
 });
 /*
-
   Card Link Interactions
-
 */
-const links = document.getElementsByClassName('js-link');
-//
-for (var l = 0; l < links.length; l++) {
-  //
-  links[l].addEventListener('mouseenter', function(){
-    var $title = this.getElementsByClassName('js-title')[0];
-    var $subtitle = this.getElementsByClassName('js-title')[1];
-    translateY(this);
-    shadow(this);
-    show($title);
-    show($subtitle);
-  });
-  //
-  links[l].addEventListener('mouseleave', function(){
-    var $title = this.getElementsByClassName('js-title')[0];
-    var $subtitle = this.getElementsByClassName('js-title')[1];
-    translateY(this);
-    shadow(this);
-    show($title);
-    show($subtitle);
-  });
+const $links = document.getElementsByClassName('js-link');
+if ($links) {
+  for (var l = 0; l < $links.length; l++) {
+    //
+    $links[l].addEventListener('mouseenter', function(){
+      var $title = this.getElementsByClassName('js-title')[0];
+      var $subtitle = this.getElementsByClassName('js-title')[1];
+      toggle(this, 'translateY-0', 'translateY--0_5');
+      toggle(this, 'shadow-0', 'shadow-1');
+      toggle($title, 'opacity-0', 'opacity-1');
+      toggle($subtitle, 'opacity-0', 'opacity-1');
+    });
+    //
+    $links[l].addEventListener('mouseleave', function(){
+      var $title = this.getElementsByClassName('js-title')[0];
+      var $subtitle = this.getElementsByClassName('js-title')[1];
+      toggle(this, 'translateY-0', 'translateY--0_5');
+      toggle(this, 'shadow-0', 'shadow-1');
+      toggle($title, 'opacity-0', 'opacity-1');
+      toggle($subtitle, 'opacity-0', 'opacity-1');
+    });
+  }
 }
 /*
-
   Case Study Source Links
-
 */
-const sources = document.getElementsByClassName('js-source');
-//
-for (var s = 0; s < sources.length; s++) {
-  //
-  sources[s].addEventListener('mouseenter', function(){
-    translateY(this);
-    shadow(this);
-  });
-  //
-  sources[s].addEventListener('mouseleave', function(){
-    translateY(this);
-    shadow(this);
-  });
+const $sources = document.getElementsByClassName('js-source');
+if ($sources) {
+  for (var s = 0; s < $sources.length; s++) {
+    $sources[s].addEventListener('mouseenter', function(){
+      toggle(this, 'translateY-0', 'translateY--0_5');
+      toggle(this, 'shadow-0', 'shadow-1');
+    });
+    $sources[s].addEventListener('mouseleave', function(){
+      toggle(this, 'translateY-0', 'translateY--0_5');
+      toggle(this, 'shadow-0', 'shadow-1');
+    });
+  }
 }
 /*
-
   Navigation Arrow Interaction
-
 */
-const nav = document.getElementsByClassName('js-nav')[0];
-//
-if (nav) {
-  nav.addEventListener('mouseenter', function(){
-    var $arrow = this.getElementsByClassName('js-arrow')[0];
-    translateX($arrow);
-  });
-  //
-  nav.addEventListener('mouseleave', function(){
-    var $arrow = this.getElementsByClassName('js-arrow')[0];
-    translateX($arrow);
-  });
+const $nav = document.getElementsByClassName('js-nav');
+if ($nav) {
+  for (var n = 0; n < $nav.length; n++) {
+    $nav[n].addEventListener('mouseenter', function(){
+      toggle(this, 'translateX-0', 'translateX--0_5');
+    });
+    $nav[n].addEventListener('mouseleave', function(){
+      toggle(this, 'translateX-0', 'translateX--0_5');
+    });
+  }
 }
 //
-function show($element) {
-  $element.classList.toggle('opacity-0');
-  $element.classList.toggle('opacity-1');
-}
-//
-function translateY($element) {
-  $element.classList.toggle('translateY-0');
-  $element.classList.toggle('translateY--0_5');
-}
-//
-function shadow($element) {
-  $element.classList.toggle('shadow-0');
-  $element.classList.toggle('shadow-1');
-}
-//
-function translateX($element) {
-  $element.classList.toggle('translateX-0');
-  $element.classList.toggle('translateX--0_5');
+function toggle($element, classA, classB) {
+  $element.classList.toggle(classA);
+  $element.classList.toggle(classB);
 }
